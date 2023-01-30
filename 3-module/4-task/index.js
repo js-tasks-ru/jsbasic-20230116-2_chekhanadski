6fd1aa6f609f6e7945763f95db65b1bf4fd57c23
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-01-30 18:12:25 +0100

    function showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  const newString = users.reduce((str, user) => {
+    if (user.age <= age) {
+      str += str.length === 0 ? `${user.name}, ${user.balance}` : `\n${user.name}, ${user.balance}`;
+    }
+    return str;
+  }, '');
+  return `${newString}`;
 }
