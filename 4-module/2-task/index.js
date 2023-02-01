f86ee674f9cebb3b027a34593c85770aa1b53b5b
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-02-01 20:54:13 +0100

    Revert "delite 4class 2task" delite the last commit This reverts commit 296bd0ea4841e31fb39abda482ee52be200112e5.

---
+++ 4-module/2-task/index.js
@@ -0,0 +1,9 @@
+function makeDiagonalRed(table) {
+
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    row.cells[i].style.backgroundColor = 'red';
+    
+    
+  }
+}
