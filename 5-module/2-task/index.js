9e59f6bbf12e124ce7f172b87726d78dbc0fd321
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-02-12 06:24:50 +0100

    Hide/Show text

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  let buttonElem = document.querySelector('.toggle-text-button'); 
+  let textElem = document.getElementById('text');  
+   
+  buttonElem.onclick = function() {
+    if (!textElem.hasAttribute('hidden')) {
+      textElem.setAttribute("hidden", "hidden");
+    } else {textElem.removeAttribute('hidden');}         
+  }; 
+      
 }
+
+
+
+
