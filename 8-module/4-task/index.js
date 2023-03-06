824d31195e22035449dc318c44be78b45ecc7585
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-03-06 15:36:46 +0100

    modul 8 task 4

--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -12,24 +12,53 @@ export default class Cart {
     this.addEventListeners();
   }
 
-  addProduct(product) {
-    // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+  addProduct(product) { 
+    
+    if (!product) {
+      return;
+    }
+    
+    let target = this.cartItems.findIndex(item => item.product.id === product.id);
+    if (target >= 0) {
+      this.cartItems[target].count += 1;
+    } else {
+      this.cartItems.push({ product: product, count: 1 });
+    }
+
+    this.onProductUpdate(this.cartItems);
   }
 
   updateProductCount(productId, amount) {
-    // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let targetIndex = this.cartItems.findIndex(item => item.product.id === productId);
+
+    if (this.cartItems[targetIndex].count > 0) {
+      let target = this.cartItems.find(item => item.product.id === productId).count += (amount);
+    }
+    if (this.cartItems[targetIndex].count === 0) {
+      this.cartItems.splice(targetIndex, 1);
+    }
+
+    this.onProductUpdate(this.cartItems);
   }
 
   isEmpty() {
-    // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let totalCount = 0;
+    for (let elem of this.cartItems) {
+      totalCount += elem.count;
+    }
+    return totalCount;
   }
 
   getTotalPrice() {
-    // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let totalPrice = 0;
+    for (let elem of this.cartItems) {
+      totalPrice += elem.product.price * elem.count;
+    }
+    return totalPrice;
   }
 
   renderProduct(product, count) {
@@ -83,22 +112,166 @@ export default class Cart {
     </form>`);
   }
 
+  // Метод отвечающий за открытие модального окна корзинки
   renderModal() {
-    // ...ваш код
+    document.body.classList.add("is-modal-open");       
+    let divModal = document.createElement("div");       // создаем новое модальное коно на основе компонента    
+    divModal.classList.add("modal");                    
+    divModal.insertAdjacentHTML("afterbegin", `         
+    <div class="modal__overlay"></div>
+    <div class="modal__inner">
+      <div class="modal__header">        
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <!--Добавляем заголовок-->
+        <h3 class="modal__title">Your order</h3>        
+      </div>
+      <div class="modal__body">
+      </div>
+    </div>
+    `);
+
+    // Добавляем divModal в конец body
+    document.body.append(divModal);                     
+
+    // Создаем верстку корзины с корневым элементом div и вставляем в тело модального окна
+    for (let elem of this.cartItems) {
+      document.querySelector(".modal__body").append(this.renderProduct(elem.product, elem.count));
+    }
+
+    document.querySelector(".modal__body").append(this.renderOrderForm());
+    document.querySelector(".modal").addEventListener("click", (event) => counter(event));
+
+    //Добавляет необходимые обработчики (или один обработчик), чтобы при кликах на кнопках +/- увеличивать/уменьшать количество товаров.
+    let counter = (event) => {
+      if (event.target.closest(".cart-counter__button_minus")) {
+        // Определить по элементу +/-, какой это товар
+        this.targetId = event.target.closest(".cart-product").getAttribute("data-product-id");
+
+        for (let elem of this.cartItems) {
+          if (this.targetId === elem.product.id) {
+            // Использовать уже существующий метод updateProductCount, чтобы обновить cartItems.
+            this.updateProductCount(this.targetId, -1);
+            // Он вызовет уже существующий метод onProductUpdate, который нужно дописать, чтобы он обновлял отображение товара в модальном окне.
+            this.onProductUpdate(this.cartItems);
+          }
+        }
+      }
+
+      if (event.target.closest(".cart-counter__button_plus")) {
+        this.targetId = event.target.closest(".cart-product").getAttribute("data-product-id");
+
+        for (let elem of this.cartItems) {
+          if (this.targetId === elem.product.id) {
+            this.updateProductCount(this.targetId, 1);
+            this.onProductUpdate(this.cartItems);
+          }
+        }
+      }
+    };
+
+    // Добавляет необходимый обработчик, чтобы при событии submit на элементе формы с классом cart-form, вызывался метод onSubmit данного компонента. 
+    if (document.querySelector(".cart-form")) {
+      document.querySelector(".cart-form").addEventListener("submit", (event) => {
+        event.preventDefault(event);
+        this.onSubmit(event);
+      });
+    }
+    document.querySelector(".modal__close").addEventListener("click", () => this.buttonClose());
+    document.addEventListener("keydown", (event) => this.keyHandler(event));
   }
 
+  // Дописываем метод onProductUpdate, чтобы он обновлял верстку при изменении количества товара cartItem
   onProductUpdate(cartItem) {
-    // ...ваш код
+    // Если модальное окно с корзиной открыто (определяем по наличию класса is-modal-open на элементе body)
+    if (document.querySelector(".is-modal-open")) {
 
+      for (let elem of this.cartItems) {
+        if (this.targetId === elem.product.id) {
+          // Элемент, который хранит количество товаров с таким productId в корзине
+          let productCount = document.querySelector(`[data-product-id="${this.targetId}"] .cart-counter__count`)
+            .textContent = elem.count;
+
+          // Элемент с общей стоимостью всех единиц этого товара
+          let productPrice = document.querySelector(`[data-product-id="${this.targetId}"] .cart-product__price`)
+            .textContent = `€${(elem.product.price * elem.count).toFixed(2)}`;
+            
+          // Элемент с суммарной стоимостью всех товаров
+          let infoPrice = document.querySelector(".cart-buttons__info-price").innerHTML = `€${this.getTotalPrice().toFixed(2)}`;
+        }
+      }
+    }
+    // Если в корзине не осталось ни одного товара.
+    if (this.cartItems.length === 0) {
+      document.body.classList.remove("is-modal-open");
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").remove();
+      }
+    }
+    
     this.cartIcon.update(this);
   }
 
+  // Метод отправления данных пользователя, во время оформления заказа.
   onSubmit(event) {
-    // ...ваш код
-  };
+    // добавляем класс is-loading к кнопке с атрибутом [type=submint]
+    document.querySelector("[type=submit]").classList.add("is-loading");
+
+    let formElem = document.querySelector(".cart-form");
+    let userFormData = new FormData(formElem);
+
+    // Сделать запрос на сервер с помощью fetch и методом POST на адрес – https://httpbin.org/post
+    let result = fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: userFormData
+    });
+
+    result.then(() => {
+      // Заменяе заголовок модального окна на Success, если отправка данных была успешной.
+      document.querySelector(".modal__title").textContent = "Success!";
+      // Удаляем все товары из массива в свойстве cartItems
+      this.cartItems = [];
+
+      // Заменяем содержимое тела модального окна на верстку:
+      document.querySelector(".modal__body").innerHTML = `
+        <div class="modal__body-inner">
+          <p>
+             Order successful! Your order is being cooked :) <br>
+             We’ll notify you about delivery time shortly.<br>
+             <img src="/assets/images/delivery.gif">
+          </p>
+        </div>`;
+    });
+  }
 
   addEventListeners() {
     this.cartIcon.elem.onclick = () => this.renderModal();
+  }  
+
+  // Закрытие модального окана по нажатию кнопки " X "
+  buttonClose() {
+    if (document.querySelector(".modal__body-inner")) {
+      this.cartIcon.update(this);
+    }
+
+    document.body.classList.remove("is-modal-open");
+    if (document.querySelector(".modal")) {
+      document.querySelector(".modal").remove();
+    }
+  }
+
+  // Закрытие модального окна по нажатию клавиши "Esc"
+  keyHandler(event) {
+    if (document.querySelector(".modal__body-inner")) {
+      this.cartIcon.update(this);
+    }
+
+    if (event.code === "Escape") {
+      document.body.classList.remove("is-modal-open");
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").remove();
+      }
+    }
   }
 }
-
